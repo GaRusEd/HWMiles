a9524b5ef45c99f1696227f11f547a7a017362f6
GaRus <hiruk0@mail.ru>
2023-05-17 09:44:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int oneMile = 20;
+        int bonusMiles = ticketPrice / oneMile;
+
+        System.out.println("Вам начисленно " + bonusMiles + " бонусных миль");
+    }
+}
